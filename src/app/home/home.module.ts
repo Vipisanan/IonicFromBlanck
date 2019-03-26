5ffd78c6f68ec2a2c99e38402bd9c22598f3d442
Vipisanan <vipisanan1@gmail.com>
2019-03-26 22:07:48 +0530

    first commit and created test component

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { HomePage } from './home.page';
+import {TestComponent} from '../test/test.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage
+      },
+      {
+        path: 'test',
+        component: TestComponent
+      }
+    ])
+  ],
+  declarations: [HomePage,
+  TestComponent
+  ]
+})
+export class HomePageModule {}
